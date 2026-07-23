6f5d23c0ba37800a0dc90a476fe50e7bb6d490f9
Tanmay Jagtap <tanmaytan316@gmail.com>
2026-07-23 23:01:36 +0530

    Fix production GLB asset path for Vercel

--- src/js/watch.js
+++ src/js/watch.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { scene } from './scene';
+import watchUrl from '../assets/models/watch.glb';
 
 export let watchModel = null;
 
